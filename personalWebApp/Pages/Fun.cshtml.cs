497cae90f51eda818b4083a7274312b903ac34dd
Josh.Tillson <Josh.Tillson@studentambassadors.com>
2020-08-23 14:46:08 -0400

    initial commit

---
+++ personalWebApp/Pages/Fun.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace personalWebApp.Pages
+{
+    public class FunModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
